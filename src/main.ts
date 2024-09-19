1836f7a132018cada698d549396a3954911a9891
Ruslan Kosenkov <kosenkovrus@gmail.com>
2024-09-19 11:27:00 +0300

    Описать класс снаряда

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,42 @@ class Player {
 }
 
 class Projecttile {
-	constructor() { }
+	width: number
+	height: number
+	x: number
+	y: number
+	speed: number
+	free: boolean
+
+	constructor() {
+		this.width = 5
+		this.height = 10
+		this.x = 0
+		this.y = 0
+		this.speed = 5
+		this.free = true
+	}
+
+	draw (context: CanvasRenderingContext2D) {
+		if (!this.free) {
+			context.fillRect(this.x, this.y, this.width, this.height)
+		}
+	}
+
+	update () {
+		if (!this.free) this.y -= this.speed
+		if (this.y < -this.height) this.reset()
+	}
+
+	start (x: number, y: number) {
+		this.x = x - this.width * 0.5
+		this.y = y
+		this.free = false
+	}
+
+	reset () {
+		this.free = true
+	}
 }
 
 class Enemy {
