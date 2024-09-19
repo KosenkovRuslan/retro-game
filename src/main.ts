d3d81c3baf9a86c191155068c82fb9294b1efebf
Ruslan Kosenkov <kosenkovrus@gmail.com>
2024-09-19 13:05:48 +0300

    Описать класс Enemy и Wave

--- src/main.ts
+++ src/main.ts
@@ -74,7 +74,83 @@ class Projecttile {
 }
 
 class Enemy {
-	constructor() { }
+	game: Game
+	width: number
+	height: number
+	x: number
+	y: number
+	positionX: number
+	positionY: number
+	
+	constructor(game: Game, positionX: number, positionY: number) {
+		this.game = game
+		this.width = game.enemySize
+		this.height = game.enemySize
+		this.x = 0
+		this.y = 0
+		this.positionX = positionX
+		this.positionY = positionY
+	}
+
+	draw(context: CanvasRenderingContext2D) {
+		context.strokeRect(this.x, this.y, this.width, this.height)
+	}
+
+	update(x: number, y: number) {
+		this.x = x + this.positionX
+		this.y = y + this.positionY
+	}
+}
+
+class Wave {
+	game: Game
+	width: number
+	height: number
+	x: number
+	y: number
+	speedX: number
+	speedY: number
+	enemies: Enemy[]
+
+	constructor(game: Game) {
+		this.game = game
+		this.width = this.game.columns * this.game.enemySize
+		this.height = this.game.rows * this.game.enemySize
+		this.x = 0
+		this.y = -this.height
+		this.speedX = 1
+		this.speedY = 0
+		this.enemies = []
+		this.create()
+	}
+
+	render (context: CanvasRenderingContext2D) {
+		if (this.y < 0) this.y += 5
+		this.speedY = 0
+
+		if (this.x < 0 || this.x > this.game.width - this.width) {
+			this.speedX *= -1	
+			this.speedY = this.game.enemySize
+		}
+
+		this.x += this.speedX
+		this.y += this.speedY
+		this.enemies.forEach(enemy => {
+			enemy.update(this.x, this.y)
+			enemy.draw(context)
+		})
+	}
+
+	create() {
+		for (let y = 0; y < this.game.rows; y++) {
+			for (let x = 0; x < this.game.columns; x++) {
+				let enemyX = x * this.game.enemySize
+				let enemyY = y * this.game.enemySize
+
+				this.enemies.push(new Enemy(this.game, enemyX, enemyY))
+			}
+		}
+	}
 }
 
 class Game {
